43ccab9baf54f3473d1a90b53334fca5676a4b9a
cliftonmb <cliftonmbutler@gmail.com>
2022-05-12 16:28:18 -0500

    add bootstrap template

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios";
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Vue from 'vue';
+Vue
+
 
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
